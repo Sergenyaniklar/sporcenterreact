3b93214afe7e1aa14bfb9dd9ec45ba82a1f1abd9
Sergenyaniklar <sergen.ynklr701@gmail.com>
2024-11-30 03:13:38 +0300

    Send

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css'
+import Header from './components/header';
+import Hero from './components/hero';
+import Classes from './components/classes';
+import BmiCalculator from './components/bmi-calculator';
+import Trainers from './components/trainers';
+import Products from './components/products';
+import Review from './components/review';
+import Contact from './components/contact';
+import Footer from './components/footer';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Classes />
+      <BmiCalculator />
+      <Trainers />
+      <Products />
+      <Review />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
